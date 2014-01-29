7f1b5cf67a778edfb1d9a39037b0bd8ab81fc382
Ajit Singh <ajitya2002@gmail.com>
2014-01-29 22:33:43 +0530

    Make few updation on code and code commenting

--- create_issue.php
+++ create_issue.php
@@ -11,7 +11,7 @@ $arguments = getopt("u:p:");
 if (count($arguments) > 0 and count($argv) > 0) {
     $serviceObj = new requestSendApi(5, 6, 7); // 5= url, 6=title, 7= description 
     $response = $serviceObj->requestApi($argv, $arguments);
-    
+
     echo $response; // displaye JSON reponse from github/bitbucket on CLI
 } else {
     echo "Kindly provide username, password, repository API url, issue title and description.";
