d8f2f04790ae2c7222ba25836696c46f8340fbb9
Ajit Singh <ajitya2002@gmail.com>
2014-01-29 22:32:21 +0530

    Make few updation on code and code commenting

--- create_issue.php
+++ create_issue.php
@@ -2,16 +2,17 @@
 
 /* * ***************************************************************************
  * Lib create issues in Github and Bitbucket repository
- * Author - Ajit Singh
+ * @Author - Ajit Singh
  * Created Date: 28/01/2014 
  * *************************************************************************** */
 include_once "request_send_api.php";
-$serviceObj = new requestSendApi();
 $argv = $GLOBALS['argv'];
 $arguments = getopt("u:p:");
 if (count($arguments) > 0 and count($argv) > 0) {
+    $serviceObj = new requestSendApi(5, 6, 7); // 5= url, 6=title, 7= description 
     $response = $serviceObj->requestApi($argv, $arguments);
-    print_r($response);
+    
+    echo $response; // displaye JSON reponse from github/bitbucket on CLI
 } else {
     echo "Kindly provide username, password, repository API url, issue title and description.";
 }
