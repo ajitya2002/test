bbdd02d6e115018ba2b9fdec7251328cad059146
Ajit Singh <ajitya2002@gmail.com>
2015-01-19 17:11:41 +0530

    Name Space used

--- create_issue.php
+++ create_issue.php
@@ -6,10 +6,12 @@
  * Created Date: 28/01/2014 
  * *************************************************************************** */
 include_once "request_send_api.php";
+use  Request\Send\Api\requestSendApi as requestSendApi;
 $argv = $GLOBALS['argv'];
 $arguments = getopt("u:p:");
 if (count($arguments) > 0 and count($argv) > 0) {
     $serviceObj = new requestSendApi(5, 6, 7); // 5= url, 6=title, 7= description 
+    
     $response = $serviceObj->requestApi($argv, $arguments);
 
     echo $response; // displaye JSON reponse from github/bitbucket on CLI
